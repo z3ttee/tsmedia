1f159d6a3b39f8f308e02e1490b98f3682d17bf4
z3ttee <cedric.zitzmann@gmail.com>
2020-08-05 14:46:01 +0200

    Added uploads

--- api/src/endpoints/v1/uploadEndpoint.php
+++ api/src/endpoints/v1/uploadEndpoint.php
@@ -32,20 +32,57 @@ class UploadEndpoint extends Endpoint {
     }
 
     /**
-     * @api {post} /upload/video Upload video
+     * @api {post} /upload/video/ Upload video
      * @apiDescription Uploads a new video file
      * @apiGroup Upload
      * @apiName Upload video
+     * 
+     * @apiParam {File} file The file to upload (8GB max).
      * 
      * @apiUse CommonDoc
      * 
      * @apiSuccess {String} id UUID of the uploaded video on success.
      * 
+     * @apiError too_large The provided file is to large.
+     * @apiError not_uploaded An error occured when saving file.
+     * @apiError unsupported_encoding The provided file has an invalid format or encoding.
+     * 
      * @apiHeader {String} Authorization User's unique access-token (Bearer).
      * @apiVersion 1.0.0
      */
     function uploadVideo() {
-        
+        if(!isset($_FILES['file']) || empty($_FILES['file'])) {
+            throw new \Exception('missing required params');
+        }
+
+        $file = $_FILES['file'];
+        $fileSize = $file['size'];
+        $uuid = uuidv4();
+        $maxSize = 1000*1000*1000*1000*8;   // 8GB max
+
+        if($fileSize > $maxSize) {
+            throw new \Exception('too large');
+        }
+
+        $mimeTypes = array('video/mpeg', 'video/mp4', 'video/ogg', 'video/webm', 'video/x-msvideo');
+        $ext = '.'.\pathinfo($file['name'],PATHINFO_EXTENSION);
+
+        if(!\in_array($file['type'], $mimeTypes)) {
+            throw new \Exception('unsupported encoding');
+        }
+
+        $target_dir = "uploads/videos/";
+        $target_file = $target_dir.$uuid.$ext;
+
+        \var_dump($target_file);
+
+        if(!\is_dir($target_dir) && !\mkdir($target_dir, 0777, true)) {
+            throw new \Exception('not uploaded');
+        }
+
+        if(!move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+            throw new \Exception('not uploaded');
+        }
     }
 
     function requiresAuthenticated() {
