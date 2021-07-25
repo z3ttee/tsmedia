496456ef7436eb0e67584606cac5943ae0d9790b
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-07-25 19:31:55 +0200

    added grid

--- app/src/views/main/AppHomeView.vue
+++ app/src/views/main/AppHomeView.vue
@@ -1,9 +1,10 @@
 <template>
     <section>
         <div class="grid-wrapper">
-            <wg-card class="grid-item" title="Videotitle" subtitle="Subtitel">
-                <template #title>Videotitel</template>
-                <template #subtitle>Videotitel</template>
+            <wg-card class="grid-item card-text" :clickable="true" v-for="i of [1,2,3,4,5,6,7,8,9,10]" :key="i">
+                <template #media><wg-image></wg-image></template>
+                <template #title>Videotitel {{ i }}</template>
+                <template #subtitle>Kekdef</template>
             </wg-card>
         </div>
     </section>
@@ -11,17 +12,24 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { WgCard } from "@tsalliance/wydget-vue"
+import { WgCard, WgImage } from "@tsalliance/wydget-vue"
 
 export default defineComponent({
     components: {
-        WgCard
+        WgCard,
+        WgImage
     }
 })
 </script>
 
 <style lang="scss" scoped>
+.grid-wrapper {
+    display: grid;
+    grid-template-columns: 20% 20% 20% 20% 20%;
+    grid-template-rows: auto;
+}
 .grid-item {
     width: 100%;
+    padding: 0.4rem;
 }
 </style>
